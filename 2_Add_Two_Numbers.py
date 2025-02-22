e6a7ca3f9f5420ccbb489c6f34bb600b6cc585e4
AMIT ACHARJEE <117907179+Grey-Coder@users.noreply.github.com>
2025-02-22 19:07:12 +0530

    Update 2_Add_Two_Numbers.py

--- 2_Add_Two_Numbers.py
+++ 2_Add_Two_Numbers.py
@@ -1,19 +1,59 @@
-def addTwoNumbers(l1,l2):
-    s1=""
-    s2=""
-    for i in range(len(l1)):
-        s1+=str(l1[i])
-    for j in range(len(l2)):
-        s2+=str(l2[j])
-    a1=str(int(s1[::-1])+int(s2[::-1]))
-    a1=a1[::-1]
-    l=[]
-    for i in range(len(a1)):
-        l.append(int(a1[i]))
-    return l
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-l1 = [9,9,9,9,9,9,9]
-l2 = [9,9,9,9]
-print(list(addTwoNumbers(l1,l2)))
+def addTwoNumbers(root1,root2):
+    head=ListNode(0)
+    root=head
+    carry=0
 
-#pending work: Convert LinkedList to Normal List -> Normal List to LinkedList
+    while root1 or root2 or carry:
+        v1=root1.val if root1 else 0
+        v2=root2.val if root2 else 0
+        s=v1+v2+carry
+
+        carry=s//10
+        head.next=ListNode(s%10)
+        head=head.next
+
+        if root1:
+            root1=root1.next
+
+        if root2:
+            root2=root2.next
+    
+    return root.next
+
+
+#input 1
+n1=input("Enter first data: ")
+n1=list(map(int,n1))
+
+#input 2
+n2=input("Enter second data: ")
+n2=list(map(int,n2))
+
+#creating first singly-linked list with the first data
+d1=len(n1)
+l1=ListNode(n1[0])
+root1=l1
+for i in range(1,d1):
+    l1.next=ListNode(n1[i])
+    l1=l1.next
+
+#creating second singly-linked list with the second data
+d2=len(n2)
+l2=ListNode(n2[0])
+root2=l2
+for i in range(1,d2):
+    l2.next=ListNode(n2[i])
+    l2=l2.next
+
+x=[]
+result=addTwoNumbers(root1,root2)
+while result:
+    x.append(result.val)
+    result=result.next
+print(x)
